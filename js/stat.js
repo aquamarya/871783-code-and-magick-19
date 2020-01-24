14e947f2f0920e6fd06768cf11d71875a626d525
Maria Vlasova <vlasova_ya@mail.ru>
2020-01-24 19:36:37 +0400

    adds the histogram

--- js/stat.js
+++ js/stat.js
@@ -4,13 +4,14 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
+var GAP = 20;
 var BAR_GAP = 50;
-var BAR_X = CLOUD_X + GAP * 2;
-var BAR_Y = CLOUD_Y + GAP * 2;
+var BAR_X = CLOUD_X + GAP;
+var BAR_Y = CLOUD_Y + GAP;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
-var barWidth = CLOUD_WIDTH - GAP - BAR_WIDTH - GAP;
+// var barWidth = CLOUD_WIDTH - GAP - BAR_WIDTH - GAP;
+var barHeight = CLOUD_HEIGHT - GAP - BAR_GAP * 2;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -32,9 +33,6 @@ var getRandomInt = function (int) {
   var randomInt = Math.floor(Math.random() * int) + 1;
   return randomInt;
 };
-// var getRandomInt = function (max) {
-//   return Math.floor(Math.random() * Math.floor(max));
-// };
 
 var getPaintBar = function (arr, ctx, i) {
   if (arr[i] === 'Вы') {
@@ -53,14 +51,13 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#1b254c';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', BAR_X, BAR_Y);
-  ctx.fillText('Список результатов:', BAR_X, BAR_Y + GAP * 2);
+  ctx.fillText('Список результатов:', BAR_X, BAR_Y + GAP);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillText(names [i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - BAR_WIDTH + BAR_GAP / 2);
     getPaintBar(names, ctx, i);
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT / 3, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
-    // ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT / 3, (BAR_HEIGHT * times[i]) / maxTime, barHeight);
   }
 };
