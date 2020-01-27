25402dd8edf12e0849a5576de920855ba7605857
Maria Vlasova <vlasova_ya@mail.ru>
2020-01-28 00:29:42 +0400

    adds array of wizards

---
+++ js/setup.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COATCOLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomItem = function (array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+var createWizards = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    array[i] = {
+      name: getRandomItem([WIZARD_NAMES]) + '' + getRandomItem([WIZARD_SURNAMES]),
+      coatColor: getRandomItem([WIZARD_COATCOLORS]),
+      eyesColor: getRandomItem([WIZARD_EYESCOLORS])
+    };
+  }
+  return array;
+};
+
+var wizards = createWizards();
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content.querySelector('.setup-similar-item');
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
