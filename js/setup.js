72d330233255478188a6a73492d7d69f7242a820
Maria Vlasova <vlasova_ya@mail.ru>
2020-01-30 20:17:28 +0400

    removes disadvantages

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,40 @@
 'use strict';
 
-var WIZARD_NAMES = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COATCOLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
-var WIZARD_EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NAMES = [
+  'Иван ',
+  'Хуан Себастьян ',
+  'Мария ',
+  'Кристоф ',
+  'Виктор ',
+  'Юлия ',
+  'Люпита ',
+  'Вашингтон '
+];
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var WIZARD_COATCOLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var WIZARD_EYESCOLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 
 var getRandomItem = function (array) {
   return Math.floor(Math.random() * array.length);
@@ -11,8 +42,8 @@ var getRandomItem = function (array) {
 
 var wizards = [];
 
-var createWizards = function () {
-  for (var i = 0; i < 4; i++) {
+var createWizards = function (length) {
+  for (var i = 0; i < length; i++) {
     wizards[i] = {
       name: WIZARD_NAMES[getRandomItem(WIZARD_NAMES)] + '' + WIZARD_SURNAMES[getRandomItem(WIZARD_SURNAMES)],
       coatColor: WIZARD_COATCOLORS[getRandomItem(WIZARD_COATCOLORS)],
@@ -22,7 +53,7 @@ var createWizards = function () {
   return wizards;
 };
 
-wizards[i] = createWizards();
+createWizards(4);
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
